37dc5855a54be75d99d4d1476418b3aeda75cbbe
Antonio Dominguez <abelardo.daguilar@gmail.com>
2020-04-08 11:37:17 -0500

    Login implementation and users register

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
+import { UsuarioService } from '../services/service.index';
+import { Usuario } from '../models/usuario.model';
 
 declare function init_plugins();
+declare const gapi: any;
 
 @Component({
   selector: 'app-login',
@@ -10,15 +14,80 @@ declare function init_plugins();
 })
 export class LoginComponent implements OnInit {
 
+  email: string;
+  recuerdame = false;
+
+  auth2: any;
+
   constructor(
     public router: Router,
+    public _usuarioService: UsuarioService
   ) { }
 
   ngOnInit(): void {
     init_plugins();
+    this.googleInit();
+
+    this.email = localStorage.getItem('email') || '';
+
+    if ( this.email.length > 1 ) {
+      this.recuerdame = true;
+    }
   }
 
-  ingresar() {
-    this.router.navigate(['/dashboard']);
+  googleInit() {
+
+    gapi.load('auth2', () => {
+
+      this.auth2 = gapi.auth2.init({
+
+        client_id: '718314167492-665i2grclnrts2ccrddmll14np4pohco.apps.googleusercontent.com',
+        cookiepolicy: 'single_host_origin',
+        scope: 'profile email'
+
+      });
+
+      this.attachSignin( document.getElementById('btnGoogle'));
+
+    });
+
   }
+
+  attachSignin( element ) {
+
+    this.auth2.attachClickHandler( element, {}, (googleUser) => {
+
+      let profile = googleUser.getBasicProfile();
+
+      let token = googleUser.getAuthResponse().id_token;
+
+      this._usuarioService.loginGoogle(token)
+              .subscribe( res => {
+
+              window.location.href = '#/dashboard';
+
+              });
+
+    });
+
+  }
+
+  ingresar( forma: NgForm ) {
+
+    if (forma.invalid) {
+      return;
+    }
+
+    let usuario = new Usuario(null, forma.value.email, forma.value.password );
+
+    this._usuarioService.login( usuario, this.recuerdame )
+            .subscribe( res => {
+
+              this.router.navigate(['/dashboard']);
+
+            });
+
+    // this.router.navigate(['/dashboard']);
+  }
+
 }
